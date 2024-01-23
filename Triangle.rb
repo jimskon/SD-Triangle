b9c8e89518157ef8d3c233d0d33c83abbe10ed30
James Skon <skonjp@gmail.com>
2024-01-23 15:32:31 -0500

    First Commit

---
+++ Triangle.rb
@@ -0,0 +1,12 @@
+
+
+
+
+
+puts(get_triangle_type([2, 6, 5])) # ➞ "scalene"
+
+puts(get_triangle_type([4, 4, 7])) # ➞ "isosceles"
+
+puts(get_triangle_type([8, 8, 8])) # ➞ "equilateral"
+
+puts(get_triangle_type([3, 5, 5, 2])) # ➞ "not a triangle"
